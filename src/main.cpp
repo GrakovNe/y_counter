2a0e60590bddf377772c30882f7eee264ceca249
grakovne <grakovne@gmail.com>
2022-05-13 20:39:33 +0300

    add ui

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,19 @@
 #include <ESP8266mDNS.h>
 #include "lib/rad_sense/radSens1v2.h"
 #include "lib/wifi_manager/WiFiManager.h"
-#include "lib/timer/ESP8266TimerInterrupt.h"
+#include "ui.h"
 
-#define TIMER_INTERVAL_2S             2000L
+#define DEVICE_NAME "ymeter"
 
-ESP8266Timer timer;
-ESP8266WebServer httpServer(8080);
+ESP8266WebServer httpServer(80);
 WiFiManager wm;
 ClimateGuard_RadSens1v2 radSens(RS_DEFAULT_I2C_ADDRESS);
 
 float staticIntensity = 0;
 float dynamicIntensity = 0;
 uint32_t pulseCounter = 0;
+
+unsigned long counter = 0;
 
 String to_json() {
     return "{"
@@ -29,20 +30,38 @@ String to_json() {
 
 void restServerRouting() {
     httpServer.on("/statistic", HTTP_GET, []() {
-        httpServer.send(200, F("application/json"),to_json());
+        httpServer.sendHeader("Access-Control-Allow-Origin", "*");
+        httpServer.send(200, F("application/json"), to_json());
     });
+
+    httpServer.onNotFound([]() {
+        httpServer.sendHeader("Access-Control-Allow-Origin", "*");
+        httpServer.send(200, F("text/html; charset=utf-8"), mainPage);
+    });
+
 }
 
 void configure_wlan() {
-    wm.setTitle("Y-Meter");
+    wm.setTitle(DEVICE_NAME);
+
+    WiFi.hostname(DEVICE_NAME);
+    wm.setHostname(DEVICE_NAME);
+    wm.setHttpPort(8080);
 
     WiFi.mode(WIFI_STA);
-    if (wm.autoConnect("radsense")) {
+    if (wm.autoConnect(DEVICE_NAME)) {
         wm.setConfigPortalBlocking(false);
         wm.startConfigPortal();
 
         restServerRouting();
         httpServer.begin();
+
+        if (MDNS.begin(DEVICE_NAME, WiFi.localIP())) {
+            Serial.println("MDNS responder started");
+        } else {
+            Serial.println("MDNS Failed");
+        }
+
     } else {
         Serial.println("Failed to connect, resetting...");
         wm.resetSettings();
@@ -74,10 +93,16 @@ void setup() {
     configure_wlan();
     init_sensor();
 
-    timer.setInterval(TIMER_INTERVAL_2S * 1000, update_and_publish);
+    counter = millis();
 }
 
 void loop() {
     wm.process();
     httpServer.handleClient();
+    MDNS.update();
+
+    if (millis() - counter >= 1000) {
+        update_and_publish();
+        counter = millis();
+    }
 }
